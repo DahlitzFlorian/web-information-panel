3c821cd11567881631ae8a2e6fea1345d064851a
florian dahlitz <f2dahlitz@freenet.de>
2017-07-08 18:09:18 +0200

    added config-file for twitter api use

---
+++ information-panel/config.py
@@ -0,0 +1,8 @@
+# dict including API key, secret and access token to connect
+# to your twitter application
+access_data = {
+    "customer_key": "",
+    "customer_secret": "",
+    "access_token": "",
+    "access_token_secret": ""
+}
