2aae460749c5366aa36b8003a5531dd5c6f9326b
florian dahlitz <f2dahlitz@freenet.de>
2017-08-01 22:08:15 +0200

    added test of fetching tweets --> missing auth

--- information-panel/semantic_analysis.py
+++ information-panel/semantic_analysis.py
@@ -3,6 +3,9 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
 import os
+from analytics.twitterapi import TwitterAPI
+from analytics.scoring import scoring
+from config import access_data as config
 
 # global variables
 location = "P:/python/information-panel/"
@@ -47,6 +50,12 @@ app.layout = html.Div(children=[
 
 def run_analysis(n_clicks, term):
     save_term(term)
+    twitterapi = TwitterAPI(config["customer_key"],
+                            config["customer_secret"],
+                            config["access_token"],
+                            config["access_token_secret"])
+    tweets = twitterapi.get_tweets("Trump", 2, lang="de")
+    print(tweets)
     delete_term_files()
 
 if __name__ == "__main__":
